5d7475664e5aa195c69252c6da9ccdeb7138b3e9
Abhishek Modi <abhishekmodi6020@gmail.com>
2019-07-03 13:07:19 -0500

    URL-based selection routing for edit and its paricular id and solving direct loading problems for it

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    console.log(this.props);
+    return <div>{this.props.stream.title}</div>;
+  }
+}
+
+// NOTE: since we have two source of information i.e state and props(seen above)
+// we use ownProps.. as mapStateToProps has 2 arguments
+const mapStateToProps = (state, ownProps) => {
+  // console.log(ownProps);
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
